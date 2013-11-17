b584ab22fad1e0c0215d14cef6e88a715b4b9d55
Chris Bunch <shatterednirvana@gmail.com>
2013-11-16 20:03:05 -0800

    added pydoc on all methods

--- justonce.py
+++ justonce.py
@@ -30,9 +30,25 @@ class OneTimeFile(ndb.Model):
 
 
 class MainPage(webapp2.RequestHandler):
+  """MainPage defines one route for downloading (GET) and uploading (POST).
+
+  Callers can upload or download to any URL, with no authentication.
+  """
 
 
   def get(self, name):
+    """Downloads the item indexed by the given name, if it exists.
+
+    Note that since the item is deleted after the download is started, if the
+    caller fails to download the item, there is no way to download it again.
+
+    Args:
+      name: A str that names the item that should be downloaded.
+
+    Returns:
+      If the item exists, its contents are printed as the response. Otherwise,
+      a 404 is returned to the user.
+    """
     one_time_file = OneTimeFile.get_by_id(name)
     if one_time_file:
       self.response.out.write(one_time_file.contents)
@@ -42,6 +58,15 @@ class MainPage(webapp2.RequestHandler):
 
 
   def post(self, name):
+    """Uploads a new item with the specified name.
+
+    This method doesn't do any validation on the name, so if another item is
+    stored with the same name, it will be overwritten.
+
+    Args:
+      name: A str that names the item that should be uploaded.
+      body: The request body, which will be stored for later retrieval.
+    """
     one_time_file = OneTimeFile(id = name)
     one_time_file.contents = self.request.body
     one_time_file.put()
